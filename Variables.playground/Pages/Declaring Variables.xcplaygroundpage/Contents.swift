1621ebf12e828f67aa0ee91acd98fa91f16a435a
Feporras <67969032+Feporras@users.noreply.github.com>
2020-07-14 22:45:38 -0500

    Add files via upload

---
+++ Variables.playground/Pages/Declaring Variables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+/*:
+ ## Declaring Variables
+ 
+ Declaring a variable is almost exactly like declaring a constant. But instead of using the keyword `let`, you use the keyword `var`:
+ */
+// Declaring a constant
+let placeOfBirth = "New Jersey"
+
+// Declaring a variable
+var currentLocation = "New Jersey"
+
+/*:
+ Once you have declared a variable, you can assign a new value to it:
+*/
+// Assigning a new value to a variable
+currentLocation = "California"
+
+/*:
+ - experiment: Below, add a line of code that assigns a new value to `currentLocation`:
+ */
+var currentlocation = "chicago"
+
